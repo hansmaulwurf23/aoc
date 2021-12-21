1094b5e79ef6290f2fca45d2981491bf8fdc32cf
unrz266 <martin.fischer@fau.de>
2021-12-21 20:15:06 +0100

    finished day7

---
+++ day7/two.groovy
@@ -0,0 +1,19 @@
+def pop = new File('./input.txt').text.tokenize(',')*.toInteger()
+def demo = [:].withDefault { 0 }
+demo.putAll(pop.countBy { it })
+println demo
+
+def calcFuel = { int pos ->
+    demo.collect { x, popSize ->
+        def steps = Math.abs(x - pos).toBigInteger()
+        (steps * (steps + 1)).divide(2).multiply(popSize)
+    }.sum()
+}
+
+def fuelByPos = [:]
+((demo.keySet().min())..(demo.keySet().max())).each { pos ->
+    fuelByPos[pos] = calcFuel(pos)
+}
+
+def minFuel = fuelByPos.values().min()
+println "min fuel $minFuel at ${fuelByPos.findAll { pos, fuel -> fuel == minFuel}}"
