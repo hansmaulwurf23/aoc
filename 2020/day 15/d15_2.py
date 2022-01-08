0ab09c6e36675ea9a415c880ebe7c410f5df1bed
unrz266 <martin.fischer@fau.de>
2022-01-08 13:15:19 +0100

    finished day 15

---
+++ 2020/day 15/d15_2.py
@@ -0,0 +1,24 @@
+input = [1, 2, 16, 19, 18, 0]
+# input = [3, 1, 2]
+
+visited = {int(x): i + 1 for i, x in enumerate(input)}
+step = len(visited)
+last = input[-1]
+visited[last] = None
+endSteps = 30000000
+
+while step < endSteps:
+    if visited[last] == None:
+        next = 0
+        visited[last] = step
+    else:
+        next = step - visited[last]
+        visited[last] = step
+
+    if next not in visited.keys():
+        visited[next] = None
+
+    step += 1
+    last = next
+
+print(last)
