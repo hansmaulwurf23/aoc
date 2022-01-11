0f896dbbeaf3884e25c1031d1138a214572aaad5
unrz266 <martin.fischer@fau.de>
2022-01-11 16:56:10 +0100

    finished day 19

--- 2020/day 19/d19_2.py
+++ 2020/day 19/d19_2.py
@@ -14,23 +14,24 @@ def flatten(t):
 def explode(ruleNo):
     if ruleNo in leafs.keys():
         return leafs[ruleNo]
-    else:
-        rule = rules[ruleNo]
 
+    rule = rules[ruleNo]
     result = '(' + '|'.join([''.join([explode(r) for r in orRules if r != ruleNo]) for orRules in rule]) + ')'
 
+    # [8] only the 8 loop is handled here, we simply allow it to match at least once but as often as needed
     if ruleNo in flatten(rule):
         result += '+'
 
     leafs[ruleNo] = result
     return result
 
+
 with open('./input.txt') as file:
     # rules
     while line := file.readline().rstrip():
-        # was '42 | 42 8' which is the same thing, this one is handled in the regex
+        # was '42 | 42 8' which is the same thing, this one is handled in the regex [8]
         if line.startswith('8: '): line = "8: 42 8"
-        # was '11: 42 31 | 42 11 31'. now we match one and two mirrored 42's and 31's and do the rest here [A]
+        # was '11: 42 31 | 42 11 31'. now we match one and two mirrored 42's and 31's and do the rest here [11]
         if line.startswith('11: '): line = "11: 42 31 | 42 42 31 31"
 
         (ruleNo, ruleDefs) = line.split(': ')
@@ -46,16 +47,18 @@ with open('./input.txt') as file:
 
     # messages
     matchingMessages = 0
-    special = leafs['42'] + leafs['42'] + leafs['31'] + leafs['31']
+    special = '^(?P<front>' + leafs['8'] + ')*(' + leafs['42'] + leafs['42'] + leafs['31'] + leafs['31'] + ')(?P<end>.*)$'
     while line := file.readline().rstrip():
         if regex.match(line):
             matchingMessages += 1
         else:
-            while len(line) and re.match(special, line):
-                # [A] we recursively replace matches of 42 31 with nothing and see if the line still exists and
-                # finally matches the big fat regex
-                line = re.sub(special, '', line)
-                if regex.match(line):
+            copy = str(line)
+            while len(copy) and re.match(special, copy):
+                # [11] since 0: 8 11 the whole line must match (8)* followed by the same number of 42's and 31's, so
+                # we recursively replace matches of 42 42 31 31 with nothing and see if the copy still exists and
+                # finally matches the big fat regex, which allows for one (42 31) and two (42 42 31 31)
+                copy = re.sub(special, '\g<front>\g<end>', copy)
+                if regex.match(copy):
                     matchingMessages += 1
 
 
