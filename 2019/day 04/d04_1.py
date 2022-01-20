e4c47132a20bf0f90f90c7ac452e9354d5c03216
unrz266 <martin.fischer@fau.de>
2022-01-20 08:32:31 +0100

    finished day 4

---
+++ 2019/day 04/d04_1.py
@@ -0,0 +1,27 @@
+import datetime
+
+begin_time = datetime.datetime.now()
+
+global_range = range(183564, 657474 + 1)
+valid_counter = 0
+for six in range(1, max(global_range) // pow(10, 5)):
+    for five in [_ for _ in range(1, 10) if _ >= six]:
+        for four in [_ for _ in range(1, 10) if _ >= five]:
+            for three in [_ for _ in range(1, 10) if _ >= four]:
+                for two in [_ for _ in range(1, 10) if _ >= three]:
+                    for one in [_ for _ in range(1, 10) if _ >= two]:
+                        digits = [six, five, four, three, two, one]
+                        passwd = sum([pow(10, i) * x for i, x in enumerate(reversed(digits))])
+
+                        # must be in range
+                        if passwd not in global_range:
+                            break
+
+                        # two digits must be the same
+                        for i, j in zip(range(5), range(1, 6)):
+                            if digits[i] == digits[j]:
+                                valid_counter += 1
+                                break
+
+print(valid_counter)
+print(datetime.datetime.now() - begin_time)
