b9173d6a7a12bb3363bec23673b1d2aac8612ac3
unrz266 <martin.fischer@fau.de>
2022-01-27 08:30:11 +0100

    finished day 11

---
+++ 2019/day 11/d11_2.py
@@ -0,0 +1,41 @@
+import datetime
+from intcode import IntCodeMachine
+from aopython import vector_add
+from collections import defaultdict
+from aopython import min_max_2d
+
+begin_time = datetime.datetime.now()
+
+# ordered so that turn +1 / -1 updates directions accordingly
+#               UP     RIGHT    DOWN    LEFT
+directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+turns = {0: -1, 1: 1}
+
+with open('./input.txt') as f:
+    program = [int(x) for x in f.readlines()[0].split(',')]
+
+vm = IntCodeMachine(program)
+direction = 0
+position = (0, 0)
+panels = defaultdict(lambda : 0)
+panels[position] = 1
+while not vm.terminated:
+    color = vm.run_to_output([panels[position]])
+    turn = vm.run_to_output([panels[position]])
+    if turn is not None and vm.terminated == False:
+        panels[position] = color
+        direction = (direction + turns[turn] + 4) % 4
+        position = tuple(vector_add(position, directions[direction]))
+
+fx, tx, fy, ty = min_max_2d(panels)
+for y in range(fy, ty + 1):
+    for x in range(fx, tx + 1):
+        if (x, y) in panels.keys() and panels[(x, y)] == 1:
+            print('#', end='')
+        else:
+            print(' ', end='')
+
+    print('')
+
+
+print(datetime.datetime.now() - begin_time)
