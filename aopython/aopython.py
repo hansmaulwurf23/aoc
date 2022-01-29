c5415a2f42e1db4034a9a6f2ae46a668995f33b9
unrz266 <martin.fischer@fau.de>
2022-01-29 12:43:34 +0100

    changes for day 12

--- aopython/aopython.py
+++ aopython/aopython.py
@@ -32,3 +32,58 @@ def vector_add(a, b):
     Adds two vectors
     """
     return [m + n for m, n in zip(a, b)]
+
+
+def vector(from_point, to_point):
+    """
+    Creates a new vector from from_point to to_point
+    """
+    return [m - n for m, n in zip(to_point, from_point)]
+
+
+def vector_inv(v):
+    return [-m for m in v]
+
+
+def vector_sgn(v):
+    """
+    Returns a new vector which holds the sign(c) for c in every dimension
+    :param v: vector [c1, c2, ...]
+    :return: new vector [sign(c1), sign(c2), ...]
+    """
+    return [sign(m) for m in v]
+
+
+def vector_abs(v):
+    """
+    Returns a new vector with all absolute values from the given vector
+    :param v: [c1, c2, ...]
+    :return: [abs(c1), abs(c2), ...]
+    """
+    return [abs(m) for m in v]
+
+
+def cmp(a, b):
+    """
+    Returns 1 if a > b, 0 if a == b, -1 if a < b
+    """
+    return (a > b) - (a < b)
+
+
+def sign(a):
+    return (a > 0) - (a < 0)
+
+
+def ceildiv(a, b):
+    return -(a // -b)
+
+
+def gcd(a, b):
+    while b != 0:
+        (a, b) = (b, a % b)
+    return a
+
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
