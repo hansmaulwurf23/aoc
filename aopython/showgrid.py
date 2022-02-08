1db9499b2a5ed7fe6fd439454f67cde60c33b6f8
unrz266 <martin.fischer@fau.de>
2022-02-08 07:48:43 +0100

    exposed s, c in scatter, added heatmap

--- aopython/showgrid.py
+++ aopython/showgrid.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 
-def show_grid(points, highlights=None, marker='s'):
+def show_grid(points, highlights=None, marker='s', c='#1f77b4', s=160):
     fig, ax = plt.subplots()
     fig.set(figwidth=10, figheight=10, dpi=100)
 
@@ -9,15 +9,20 @@ def show_grid(points, highlights=None, marker='s'):
 
     x = list(map(lambda x: x[0], points))
     y = list(map(lambda x: x[1], points))
-    plt.scatter(x, y, s=160, marker=marker)
+    plt.scatter(x, y, s=s, c=c, marker=marker)
 
     if highlights:
         x = list(map(lambda x: x[0], highlights))
         y = list(map(lambda x: x[1], highlights))
-        plt.scatter(x, y, s=160, c='r', marker=marker)
+        plt.scatter(x, y, s=s, c='r', marker=marker)
 
 
     plt.grid(True)
     plt.grid(True, which='minor')
     plt.show()
+
+
+def show_heatmap(points, highlights=None, marker='s', s=160):
+    colors = list(map(lambda x: x[2], points))
+    show_grid(points, highlights=highlights, marker=marker, c=colors, s=s)
 
