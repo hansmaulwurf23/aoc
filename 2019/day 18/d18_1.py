fc78a2c7d0cce11889efb9b9bedb6c8cc0f1fa80
unrz266 <martin.fischer@fau.de>
2022-02-13 14:14:13 +0100

    added keys to keysets to reduce states

--- 2019/day 18/d18_1.py
+++ 2019/day 18/d18_1.py
@@ -5,7 +5,7 @@ import showgrid
 from aopython import vector_add
 
 begin_time = datetime.datetime.now()
-moves = { (0, 1), (0, -1), (+1, 0), (-1, 0) }
+moves = {(0, 1), (0, -1), (+1, 0), (-1, 0)}
 
 walls = set()
 keys = dict()
@@ -14,16 +14,31 @@ key_loc_to_name = dict()
 door_loc_to_name = dict()
 origin = None
 
-def traverse_breadth_first(root, graph, doors, keys):
-    found_keys = []
+
+def traverse_breadth_first(root, graph, doors, keysets, rest_keys):
+    found_keysets = []
     q = deque()
     visited = set()
     visited.add(root)
     q.appendleft((root, 0))
     while len(q):
         node, steps = q.pop()
-        if node in key_loc_to_name and key_loc_to_name[node] in keys:
-            found_keys.append((node, steps))
+        if node in key_loc_to_name and key_loc_to_name[node] in rest_keys:
+            found_keysets.append(([key_loc_to_name[node]], node, steps))
+        if node in keysets:
+            ks_keys, rtsteps, alt_end, altsteps = keysets[node]
+            if ks_keys[0] in rest_keys:
+                if alt_end:
+                    found_keysets.append((ks_keys, alt_end, steps + altsteps))
+                else:
+                    found_keysets.append((ks_keys, node, steps + rtsteps))
+                for k in [keys[k] for k in ks_keys]:
+                    visited.add(k)
+            if alt_end:
+                if alt_end not in visited:
+                    visited.add(alt_end)
+                    q.appendleft((alt_end, steps + altsteps))
+
         for a, distance in graph[node]:
             if a in doors:
                 continue
@@ -31,10 +46,11 @@ def traverse_breadth_first(root, graph, doors, keys):
                 visited.add(a)
                 q.appendleft((a, steps + distance))
 
-    return found_keys
+    return found_keysets
 
 
 def adjacents(pos, walls):
+    """generator for all adjacents (no wall!) of a given position"""
     for m in moves:
         a = tuple(vector_add(pos, m))
         if a not in walls:
@@ -42,6 +58,7 @@ def adjacents(pos, walls):
 
 
 def adjacent_list(pos, walls, last_pos=None):
+    """returns all adjacents of a given pos which are not wall and optional not the given last_pos"""
     res = []
     for m in moves:
         a = tuple(vector_add(pos, m))
@@ -62,21 +79,16 @@ def apply_keys_to_doors(collected_keys, doors):
     return res
 
 
-def possible_new_paths(cur_pos, graph, doors, keys, collected_keys):
-    new_paths = []
-    found_keys = traverse_breadth_first(cur_pos, graph, apply_keys_to_doors(collected_keys, doors), keys)
-    for key_coords, steps in found_keys:
-        new_paths.append((steps, key_coords))
-
-    return new_paths
+def possible_new_paths(cur_pos, graph, doors, keysets, rest_keys, collected_keys):
+    return traverse_breadth_first(cur_pos, graph, apply_keys_to_doors(collected_keys, doors), keysets, rest_keys)
 
 
 def get_key_door_dependencies(root, walls, doors, keys):
     q = deque()
     visited = set()
     visited.add(root)
-    q.appendleft((root, 0, set()))
-    key_door_deps = defaultdict(set)
+    q.appendleft((root, 0, []))
+    key_door_deps = defaultdict(list)
     while len(q):
         node, steps, on_the_way_doors = q.pop()
         adjs = adjacents(node, walls)
@@ -84,20 +96,20 @@ def get_key_door_dependencies(root, walls, doors, keys):
             if a not in visited:
                 visited.add(a)
                 if a in doors:
-                    q.appendleft((a, steps + 1, set(on_the_way_doors | set([a]))))
+                    q.appendleft((a, steps + 1, on_the_way_doors + [a]))
                 else:
                     q.appendleft((a, steps + 1, on_the_way_doors))
                 if a in keys:
-                    key_door_deps[a] |= on_the_way_doors
+                    key_door_deps[a] += on_the_way_doors
 
     names = dict()
     for k_pos, deps in key_door_deps.items():
-        names[key_loc_to_name[k_pos]] = set(map(lambda x: door_loc_to_name[x], deps))
+        names[key_loc_to_name[k_pos]] = tuple(map(lambda x: door_loc_to_name[x], deps))
     return names
 
 
-# find all coords of dead ends
 def get_dead_ends(walls, root):
+    """find all coordinates of dead ends"""
     q = deque()
     visited = set()
     visited.add(root)
@@ -119,9 +131,9 @@ def get_dead_ends(walls, root):
     return dead_ends
 
 
-# given the dead ends, go back as long as no gem (doors or keys) or crossing (multiple adjacents) are found
-# and make convert the way back to a wall
 def process_dead_ends(walls, dead_ends, keys):
+    """starting from all dead ends, go back as long as no key or crossing (multiple adjacents) are found and convert
+    the way back to a wall """
     for d in dead_ends:
         if d in keys:
             continue
@@ -164,6 +176,57 @@ def generate_graph(root, walls, keys, doors):
     return graph
 
 
+def apply_key_door_deps_to_graph(graph, doors, keys, key_door_deps):
+    todos = defaultdict(list)
+    for k, locking_doors in key_door_deps.items():
+        if len(locking_doors):
+            todos[locking_doors].append(k)
+
+    last_door_loot = dict()
+
+    # if multiple keys depend on a same set of doors
+    for tododoors, todokeys in [(d, k) for d, k in todos.items() if len(k) > 1]:
+        last_door = doors[tododoors[-1]]
+        sum_steps = 0
+        door_furhter_down = None
+        q = deque()
+        q.appendleft(last_door)
+        visited = set()
+        visited.add(last_door)
+        while len(q):
+            node = q.pop()
+            for a, steps in graph[node]:
+                if a not in visited:
+                    if a in door_loc_to_name.keys():
+                        if door_loc_to_name[a] not in tododoors:
+                            door_furhter_down = a
+                        continue
+                    visited.add(a)
+                    sum_steps += steps
+                    q.appendleft(a)
+
+        if door_furhter_down:
+            # area of same door dependent keys is between two doors! we need to create two alternative ends
+            # of this keys: last_door and the door that ended the loop. if keys in this set lie behind crossings
+            # we must not calculate the steps twice (for the alternative going back).
+            going_back_steps = 0
+            for dead_end_key_pos in [keys[k] for k in todokeys if len(graph[keys[k]]) == 1]:
+                last_pos = None
+                while len(graph[dead_end_key_pos]) <= 2:
+                    a, steps = next((a, steps) for a, steps in graph[dead_end_key_pos] if a != last_pos)
+                    going_back_steps += steps
+                    last_pos = dead_end_key_pos
+                    dead_end_key_pos = a
+
+            last_door_loot[last_door] = (todokeys, sum_steps * 2, door_furhter_down, sum_steps + going_back_steps)
+        else:
+            # same door dependency keys in dead end area so collecting all and going back -> * 2
+            last_door_loot[last_door] = (todokeys, sum_steps * 2, door_furhter_down, 0)
+        print(f'keys frozen to set {todokeys} with door dependency {tododoors}')
+
+    return last_door_loot
+
+
 with open('./input.txt') as f:
     y = 0
     while line := f.readline().rstrip():
@@ -181,7 +244,6 @@ with open('./input.txt') as f:
 
         y += 1
 
-
 # showgrid.show_grid(walls, highlights={'r':doors.values(), 'y':keys.values()}, s=36)
 dead_ends = get_dead_ends(walls, origin)
 # showgrid.show_grid(walls, highlights={'r': dead_ends, 'violet':doors.values(), 'y':keys.values()}, s=36)
@@ -191,6 +253,7 @@ process_dead_ends(walls, dead_ends, set(keys.values()))
 # exit(0)
 key_door_deps = get_key_door_dependencies(origin, walls, set(doors.values()), set(keys.values()))
 graph = generate_graph(origin, walls, set(keys.values()), set(doors.values()))
+keysets = apply_key_door_deps_to_graph(graph, doors, keys, key_door_deps)
 # showgrid.show_grid(list(graph.keys()) + [[0,0],[80,80]], s=36)
 
 
@@ -213,17 +276,14 @@ while len(pq) > 0:
         print(cur_steps)
         break
 
-    for steps, key_pos in possible_new_paths(cur_pos, graph, doors, rest_keys, collected_keys):
-        collected_key = key_loc_to_name[key_pos]
-        new_rest_keys = list(rest_keys)
-        new_rest_keys.remove(collected_key)
-
-        new_collected_keys = tuple(collected_keys + tuple(collected_key))
+    for found_keys, last_pos, steps in possible_new_paths(cur_pos, graph, doors, keysets, rest_keys, collected_keys):
+        new_rest_keys = tuple(k for k in rest_keys if k not in found_keys)
+        new_collected_keys = tuple(collected_keys + tuple(found_keys))
         if new_collected_keys in visited:
             continue
 
-        heapq.heappush(pq, (cur_steps + steps, key_pos, new_collected_keys, new_rest_keys))
+        heapq.heappush(pq, (cur_steps + steps, last_pos, new_collected_keys, new_rest_keys))
 
-
+print(cur_steps, cur_pos, collected_keys, rest_keys)
 print('4456 <')
 print(datetime.datetime.now() - begin_time)
