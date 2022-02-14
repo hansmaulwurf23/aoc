50798276c069cee432f1e062b3a6f7ac57e06c62
unrz266 <martin.fischer@fau.de>
2022-02-14 09:35:55 +0100

    exposed markersize to hightlights

--- aopython/showgrid.py
+++ aopython/showgrid.py
@@ -7,7 +7,7 @@ def scatter(points, *, marker='s', c='#1f77b4', s=160):
     plt.scatter(x, y, s=s, c=c, marker=marker)
 
 
-def show_grid(points, highlights=None, marker='s', c='#1f77b4', s=160, minTicks=True):
+def show_grid(points, highlights=None, marker='s', c='#1f77b4', s=160, minTicks=True, highlightsize=15):
     fig, ax = plt.subplots()
     fig.set(figwidth=10, figheight=10, dpi=100)
 
@@ -17,14 +17,14 @@ def show_grid(points, highlights=None, marker='s', c='#1f77b4', s=160, minTicks=
     scatter(points, marker=marker, s=s, c=c)
 
     if isinstance(highlights, list) or isinstance(highlights, set):
-        scatter(highlights, marker=marker, s=s, c='r')
+        scatter(highlights, marker=marker, s=(highlightsize or s), c='r')
     elif isinstance(highlights, dict):
         for color, points in highlights.items():
             if isinstance(points, dict):
                 for k, coords in points.items():
-                    scatter([coords], marker=f'${k}$', s=s, c=color)
+                    scatter([coords], marker=f'${k}$', s=(highlightsize or s), c=color)
             else:
-                scatter(points, marker=marker, s=s, c=color)
+                scatter(points, marker=marker, s=(highlightsize or s), c=color)
 
 
     plt.grid(True)
