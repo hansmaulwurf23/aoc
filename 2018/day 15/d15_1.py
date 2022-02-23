c634283ad8d88405b6556d4abd3b492e905f45e3
unrz266 <martin.fischer@fau.de>
2022-02-23 14:38:07 +0100

    working on 15

--- 2018/day 15/d15_1.py
+++ 2018/day 15/d15_1.py
@@ -1,7 +1,9 @@
 import datetime
+import math
 from collections import namedtuple, defaultdict, deque
 from itertools import product
 
+import showgrid
 from aopython import vector_add
 
 begin_time = datetime.datetime.now()
@@ -11,9 +13,10 @@ elves = dict()
 goblins = dict()
 walls = set()
 
+
 def unit_turn(u, allies, targets):
     target = None
-    possible_moves = {}
+    possible_moves = set()
     for t in sorted(targets.keys()):
         if is_adjacent(u, t) and (target is None or targets[target].HP > targets[t].HP):
             target = t
@@ -22,7 +25,17 @@ def unit_turn(u, allies, targets):
 
     # no target is immediately adjacent -> move move ya!
     if not target and possible_moves:
-        pass
+        move_target, *_ = nearest_target(u, possible_moves)
+        if move_target is None:
+            # cannot move and have no target -> my turn is done
+            return
+
+        new_pos = step_to(u, move_target)
+        allies[new_pos] = allies[u]
+        del allies[u]
+        u = new_pos
+        if is_adjacent(u, move_target):
+            target = move_target
 
     if target:
         targets[target].HP -= allies[u].AP
@@ -32,10 +45,22 @@ def unit_turn(u, allies, targets):
 
 def combat_round():
     # all uni coordinates in reading order
-    unit_coords = sorted(list(elves.keys()) + list(goblins.keys()))
+    unit_coords = sorted(list(elves.keys()) + list(goblins.keys()), key=lambda t: (t[1], t[0]))
     for u in unit_coords:
-        targets = goblins if u in elves else elves
-        unit_turn(u, targets)
+        allies, targets = (elves, goblins) if u in elves else (goblins, elves)
+        unit_turn(u, allies, targets)
+
+
+def step_to(root, target):
+    if root[0] == target[0]:
+        d = (0, 1) if target[1] > root[1] else (0, -1)
+    elif root[1] == target[1]:
+        d = (1, 0) if target[0] > root[0] else (-1, 0)
+    else:
+        # FIXME move vertically or horizontally first?
+        d = (0, 1) if target[1] > root[1] else (0, -1)
+
+    return tuple(vector_add(root, d))
 
 
 def nearest_target(root, targets):
@@ -44,25 +69,31 @@ def nearest_target(root, targets):
     visited.add(root)
     q.appendleft((root, 0))
     found = []
-    min_distance = None
+    min_distance = math.inf
     while q:
         node, steps = q.pop()
-        adjs = adjacents(node)
 
-        if node in targets:
+        if node in targets and min_distance >= steps:
             found.append((node, steps))
-
-        # cannot be better?
-        if min_distance is not None and min_distance <= steps:
+            min_distance = steps
             continue
 
-        for a in adjs:
+        # cannot be better?
+        if min_distance <= steps:
+            continue
+
+        for a in adjacents(node):
+            if (a in elves or a in goblins) and a not in targets:
+                continue
             if a not in visited:
                 visited.add(a)
                 q.appendleft((a, steps + 1))
 
-    target, steps = min(found, key=found.get(0))
-    return target, steps
+    if found:
+        target, steps = min(found, key=lambda f: (f[0][1], f[0][0]))
+        return target, steps
+    else:
+        return None, None
 
 
 def is_adjacent(a, b):
@@ -76,9 +107,9 @@ def adjacents(pos, only_free_space=False):
     res = []
     for m in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         x, y = tuple(vector_add(pos, m))
-        if only_free_space and ((y, x) in elves or (y, x) in goblins):
+        if only_free_space and ((x, y) in elves or (x, y) in goblins):
             continue
-        if (y, x) not in walls:
+        if (x, y) not in walls:
             res.append((x, y))
 
     return res
@@ -89,12 +120,15 @@ with open('./input.txt') as f:
     while line := f.readline().rstrip():
         for x, c in enumerate(line):
             if c == '#':
-                walls.add((y, x))
+                walls.add((x, y))
             elif c == 'G':
-                goblins[(y, x)] = Unit(HP=300, AP=3)
+                goblins[(x, y)] = Unit(HP=300, AP=3)
             elif c == 'E':
-                elves[(y, x)] = Unit(HP=300, AP=3)
+                elves[(x, y)] = Unit(HP=300, AP=3)
 
-            y += 1
+        y += 1
 
+# showgrid.show_grid(walls, {'g': elves.keys(), 'r': goblins.keys()})
+combat_round()
+showgrid.show_grid(walls, {'g': elves.keys(), 'r': goblins.keys()})
 print(datetime.datetime.now() - begin_time)
