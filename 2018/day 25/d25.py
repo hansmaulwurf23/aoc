44e830c1ca650caa8953c80c0bfd0db4ee2969c1
unrz266 <martin.fischer@fau.de>
2022-03-08 07:21:03 +0100

    finished day 25

---
+++ 2018/day 25/d25.py
@@ -0,0 +1,56 @@
+import datetime
+from functools import reduce
+from itertools import combinations
+from collections import defaultdict
+from aopython import manhattan_distance
+
+begin_time = datetime.datetime.now()
+
+points = []
+distances = defaultdict(dict)
+with open('./input.txt') as f:
+    while line := f.readline().rstrip():
+        points.append(tuple(map(int, line.split(','))))
+
+print(f'read {len(points)} points')
+for a, b in combinations(sorted(points), 2):
+    distances[a][b] = manhattan_distance(a, b)
+    distances[b][a] = distances[a][b]
+
+print(f'calculated {len(distances)} ({sum([len(s) for s in distances.values()])}) distances')
+constellations = set()
+for p, others in distances.items():
+    cur_const = {p} | {o for o in others.keys() if others[o] <= 3}
+    # merged = False
+    # for c in constellations:
+    #     if c & cur_const:
+    #         newc = c | cur_const
+    #         constellations.remove(c)
+    #         constellations.add(newc)
+    #         merged = True
+    #         break
+    #
+    # if not merged:
+    constellations.add(frozenset(cur_const))
+
+
+while True:
+    # print(f'{len(reduce(lambda a, b: a | b, constellations))} distinct stars in {len(constellations)} constellations')
+    merged = False
+    for a, b in combinations(constellations, 2):
+        if a & b:
+            if a < b:
+                constellations.remove(a)
+            elif b < a:
+                constellations.remove(b)
+            else:
+                constellations.add(frozenset(a | b))
+                constellations.remove(a)
+                constellations.remove(b)
+            merged = True
+            break
+    if not merged:
+        break
+
+print(len(constellations))
+print(datetime.datetime.now() - begin_time)
