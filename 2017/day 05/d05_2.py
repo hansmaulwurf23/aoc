a51bc4bcb5d4fff44bfdd3d0ca1c9905a08c8444
unrz266 <martin.fischer@fau.de>
2022-03-10 06:31:25 +0100

    finished day 5

---
+++ 2017/day 05/d05_2.py
@@ -0,0 +1,23 @@
+import datetime
+
+begin_time = datetime.datetime.now()
+
+
+def process(jumps):
+    cur_idx = 0
+    steps = 0
+    while 0 <= cur_idx < len(jumps):
+        steps += 1
+        next_idx = jumps[cur_idx] + cur_idx
+        jumps[cur_idx] = jumps[cur_idx] + 1 if jumps[cur_idx] < 3 else jumps[cur_idx] - 1
+        cur_idx = next_idx
+
+    return steps
+
+jumps = []
+with open('./input.txt') as f:
+    while line := f.readline().rstrip():
+        jumps.append(int(line))
+
+print(process(jumps))
+print(datetime.datetime.now() - begin_time)
