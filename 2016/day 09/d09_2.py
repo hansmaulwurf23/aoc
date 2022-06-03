51df4438ca12b6a9d711844848b159c9f2039b43
unrz266 <martin.fischer@fau.de>
2022-06-03 12:49:20 +0200

    finished day 9

---
+++ 2016/day 09/d09_2.py
@@ -0,0 +1,31 @@
+import datetime
+import re
+begin_time = datetime.datetime.now()
+
+
+def decode(line):
+    result = []
+    i = 0
+    while i < len(line):
+        if line[i] == '(':
+            rep = re.findall(r'\(\d+x\d+\)', line[i:])[0]
+            i += len(rep)
+            chrs, repeat = list(map(int, rep[1:-1].split('x')))
+            decoded = decode(line[i:i+chrs])
+            for r in range(repeat):
+                result.append(decoded)
+            i += chrs
+        else:
+            result.append(line[i])
+            i += 1
+
+    return ''.join(result)
+
+
+sum_size = 0
+with open('./input.txt') as f:
+    while line := f.readline().rstrip():
+        sum_size += len(decode(line))
+
+print(sum_size)
+print(datetime.datetime.now() - begin_time)
