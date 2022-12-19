4de2c9db14fbec1f40fd0d0e46ce438bd977c5e8
unrz266 <martin.fischer@fau.de>
2022-12-19 17:17:31 +0100

    finished day 19 part 1

--- 2022/day 19/d19_1.py
+++ 2022/day 19/d19_1.py
@@ -11,7 +11,9 @@ RESNAMES = ['ORE', 'CLAY', 'OBSIDIAN', 'GEODE']
 RUNTIME = 24
 MAX_GEO = [(minute - 1) * minute // 2 for minute in range(RUNTIME + 1)]
 cur_best = 0
-
+cache = dict()
+cache_hit = 0
+cache_miss = 0
 
 def calc_build_possibilities(bp, resources, robots, max_cost):
     possibles = []
@@ -23,22 +25,28 @@ def calc_build_possibilities(bp, resources, robots, max_cost):
     if cst[OBSIDIAN][CLAY] <= resources[CLAY] and cst[OBSIDIAN][ORE] <= resources[ORE] and \
             max_cost[OBSIDIAN] > robots[OBSIDIAN]:
         possibles.append(OBSIDIAN)
-        # return [OBSIDIAN]
 
     if cst[CLAY][ORE] <= resources[ORE] and robots[CLAY] < max_cost[CLAY]:
         possibles.append(CLAY)
 
-    if cst[ORE][ORE] <= resources[ORE]: # and robots[ORE] < max_cost[ORE]:
+    if cst[ORE][ORE] <= resources[ORE]:
         possibles.append(ORE)
 
-    #if not possibles: # or (possibles[0] != OBSIDIAN and len(possibles) == 1):
+    #if not possibles or (possibles[0] != OBSIDIAN and len(possibles) <= 2):
     possibles.append(None)
 
     return possibles
 
 
 def run_plan(bp, robots, build_robot, resources, time_left, max_cost):
-    global cur_best
+    global cur_best, cache, cache_hit, cache_miss
+
+    if (tuple(robots), tuple(resources), time_left) in cache:
+        cache_hit += 1
+        return cache[(tuple(robots), tuple(resources), time_left)]
+    else:
+        cache_miss += 1
+
     # print(f'\n== Minute {RUNTIME - time_left + 1} {build_plan[0]} {resources} ==')
     if build_robot is not None:
         for res_type, res_count in bp[COST][build_robot].items():
@@ -53,25 +61,30 @@ def run_plan(bp, robots, build_robot, resources, time_left, max_cost):
        return 0
 
     if time_left:
-        return simulate_blueprint(bp, robots, resources, time_left, max_cost)
+        v = simulate_blueprint(bp, robots, resources, time_left, max_cost)
+        cache[(tuple(robots), tuple(resources), time_left)] = v
+        return v
     else:
         if resources[GEODE] and (cur_best is None or resources[GEODE] > cur_best):
-            print(resources)
             cur_best = resources[GEODE]
+        cache[(tuple(robots), tuple(resources), time_left)] = resources[GEODE]
         return resources[GEODE]
 
 
 def simulate_blueprint(bp, robots, resources, time_left, max_cost):
     possibilities = calc_build_possibilities(bp, resources, robots, max_cost)
+    # if time_left == 10:
+    #     print(f'cache hits {cache_hit} miss {cache_miss} ({round(cache_hit / cache_miss * 100, 2)})')
     return max(map(lambda p: run_plan(bp, robots.copy(), p, resources.copy(), time_left, max_cost), possibilities))
 
 
 def calc_blueprint_quality(bp):
-    global cur_best
-
+    global cur_best, cache, cache_hit
+    cache = dict()
     robots = [1, 0, 0, 0]
     resources = [0, 0, 0, 0]
     cur_best = None
+    cache_hit = 0
     max_cost = [max(map(lambda c: c[r] if r in c else 0, bp[COST].values())) for r in range(len(RESNAMES))]
     return simulate_blueprint(bp, robots, resources, RUNTIME, max_cost)
 
@@ -88,7 +101,9 @@ with open('./input.txt') as f:
         }))
 
 
-# print(calc_blueprint_quality(blueprints[8]))
+# print(calc_blueprint_quality(blueprints[8]), 3)
+# print(calc_blueprint_quality(blueprints[13]), 6)
+# print(calc_blueprint_quality(blueprints[22]), 4)
 
 qualis = []
 for i, bp in enumerate(blueprints):
