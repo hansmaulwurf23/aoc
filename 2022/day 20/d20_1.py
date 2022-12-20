5ccecab27b49824e04a162aa5f0853cc3695f318
unrz266 <martin.fischer@fau.de>
2022-12-20 07:59:06 +0100

    finished day 20

---
+++ 2022/day 20/d20_1.py
@@ -0,0 +1,37 @@
+import datetime
+from collections import deque
+
+begin_time = datetime.datetime.now()
+values = deque()
+
+
+def mix():
+    global values, indexes
+    for i in range(length):
+        # since numbers need to be processed in appearing order we need to find the current offset
+        offset = indexes.index(i)
+        # rotate index and value to back
+        indexes.rotate(-offset - 1)
+        values.rotate(-offset - 1)
+        # remove from back of deque, rotate to front and re-append
+        val = values.pop()
+        values.rotate(-val)
+        values.append(val)
+        # update the index position as well
+        idx = indexes.pop()
+        indexes.rotate(-val)
+        indexes.append(idx)
+
+
+
+with open('./input.txt') as f:
+    while line := f.readline().rstrip():
+        values.append(int(line))
+
+length = len(values)
+indexes = deque(range(length))
+mix()
+
+offset = values.index(0)
+print(sum([values[(offset + (i + 1) * 1000) % length] for i in range(3)]))
+print(datetime.datetime.now() - begin_time)
