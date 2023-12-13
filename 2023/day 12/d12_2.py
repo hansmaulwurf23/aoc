f7b63feebc948225269b72cab307a7e0d81cbd1b
unrz266 <martin.fischer@fau.de>
2023-12-13 10:25:21 +0100

    cleanup

--- 2023/day 12/d12_2.py
+++ 2023/day 12/d12_2.py
@@ -1,38 +1,7 @@
 import datetime
-from collections import deque
 from functools import cache
-from itertools import permutations, product
 
 begin_time = datetime.datetime.now()
-
-
-def fold(pattern, n: int):
-    return '?'.join([pattern] * n)
-
-def tokenize(pattern: str):
-    return list(filter(lambda l: l, pattern.split('.')))
-
-def min_max_counts(tok_list: list):
-    return [deque([e.count('#') for e in tok_list]),
-            deque([len(e) for e in tok_list])]
-
-def is_valid(pattern: str, grouping):
-    t = deque(grouping)
-    gl = 0
-    for i, c in enumerate(pattern):
-        if c == '.':
-            if gl:
-                if not t or t[0] != gl:
-                    return False
-                else:
-                    gl = 0
-                    t.popleft()
-        elif c == '#':
-            gl += 1
-        elif c == '?':
-            return None
-
-    return not gl if not t else t[0] == gl and len(t) == 1
 
 
 @cache
@@ -54,38 +23,6 @@ def arrangements(remaining_springs, remaining_groups):
                 cntr += arrangements(remaining_springs[len(first_group):], remaining_groups[1:])
 
     return cntr
-
-
-@cache
-def pattern_permutations(pattern: str):
-    @cache
-    def perm(pattern: str):
-        if '?' in pattern:
-            return perm(pattern.replace('?', '.', 1)) + \
-                   perm(pattern.replace('?', '#', 1))
-        else:
-            return [pattern]
-
-    ps = perm(pattern)
-    # print(ps)
-    res = []
-    for t in [tokenize(p) for p in ps]:
-        res.append([x.count('#') for x in t])
-    return res
-
-def count_arrs(pattern: list, g):
-    tokened_perms = [pattern_permutations(p) for p in pattern]
-
-    def count(tokened, used, g):
-        if len(used) > len(g):
-            return 0
-
-        if not tokened:
-            return 1 if tuple(used) == g else 0
-
-        return sum([count(tokened[1:], used + p, g) for p in tokened[0]])
-
-    return count(tokened_perms, [], g)
 
 
 infos = []
