55c27e6634585e5121590cbaf5a4f3e2bcfc5a98
unrz266 <martin.fischer@fau.de>
2024-12-06 12:17:44 +0100

    finished day 06 (refactored)

--- 2024/day 06/d06.py
+++ 2024/day 06/d06.py
@@ -9,12 +9,7 @@ DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 INVDIRS = [(0, 1), (-1, 0), (0, -1), (1, 0)]
 
 
-def is_inside(pos):
-    x, y = pos
-    return 0 <= x < DIMX and 0 <= y < DIMY
-
-
-def find_next_obstacle(pos, dir, xob, yob):
+def find_next_obstacle(pos: tuple, dir, xob, yob):
     dirobs, dim = (xob, 1) if dir % 2 == 0 else (yob, 0)
     inverse_dim = (dim + 1) % 2
     heading = DIRS[dir][dim]
@@ -33,22 +28,21 @@ def find_next_obstacle(pos, dir, xob, yob):
     return ob, steps, last_pos
 
 
-def walk(start, dir, xob, yob):
-    curpos = list(start)
-    curdir = dir
+def walk(pos, dir, xob, yob):
     turning_points = set()
+    step_sum = 0
 
-    ob, steps, last_pos = find_next_obstacle(curpos, curdir, xob, yob)
-    step_sum = steps
-    while ob is not None:
-        curdir = (curdir + 1) % 4
-        curpos = tuple(last_pos)
-        if steps > 0 and curpos in turning_points:
-            return None, True
-        turning_points.add(curpos)
-
-        ob, steps, last_pos = find_next_obstacle(curpos, curdir, xob, yob)
+    while True:
+        ob, steps, last_pos = find_next_obstacle(pos, dir, xob, yob)
         step_sum += steps
+        if ob is None:
+            break
+
+        dir = (dir + 1) % 4
+        pos = tuple(last_pos)
+        if steps > 0 and pos in turning_points:
+            return None, True
+        turning_points.add(pos)
 
     return step_sum, False
 
@@ -58,26 +52,22 @@ with open('./input.txt') as f:
     lines = list(map(lambda l: l.rstrip(), f.readlines()))
 
 DIMX, DIMY = len(lines[0]), len(lines)
-for y, line in enumerate(lines):
-    for x, c in enumerate(line):
-        if c == '#':
-            obstacles.add((x, y))
-        elif c == '^':
-            start = (x, y)
+xob, yob = defaultdict(list), defaultdict(list)
+for x, y, c in [(x, y, c) for y, l in enumerate(lines) for x, c in enumerate(l)]:
+    if c == '#':
+        obstacles.add((x, y))
+        xob[x].append(y)
+        yob[y].append(x)
+    elif c == '^':
+        start = (x, y)
 
-xob, yob, pos = defaultdict(list), defaultdict(list), tuple(start)
-for x, y in obstacles:
-    xob[x].append(y)
-    yob[y].append(x)
-
-seen, path, curdir = {pos}, [], 0
-path = []
+pos, seen, path, curdir = tuple(start), {start}, [], 0
 while True:
     nxt_pos = tuple(vector_add(pos, DIRS[curdir]))
     if nxt_pos in obstacles:
         curdir = (curdir + 1) % 4
     else:
-        if not is_inside(nxt_pos):
+        if not (0 <= nxt_pos[0] < DIMX and 0 <= nxt_pos[1] < DIMY):
             break
         pos = nxt_pos
         seen.add(pos)
